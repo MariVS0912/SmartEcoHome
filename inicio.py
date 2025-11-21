bfc895f8ca8b87a9bd3fa067677da531485df72f
MariVS0912 <mvargass3@eafit.edu.co>
2025-11-21 07:37:35 -0500

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -7,18 +7,14 @@ st.title("SmartEcoHome 🌱")
 st.markdown("""
 Bienvenida/o a SmartEcoHome — proyecto final de Interfaces Multimodales.
 
-Navega a **Control** para interactuar por botones/texto, a **Voz** para enviar comandos por audio (sube un archivo .wav/.mp3), o a **Imagen** para detectar personas desde una foto.
 """)
 
 st.header("Objetivos del proyecto")
 st.markdown("""
-- Interacción multimodal: texto, voz (archivo), imagen, botones.  
+- Interacción multimodal.  
 - Comunicación con ESP32 (simulado en WOKWI) por MQTT.  
 - 2+ páginas en Streamlit.  
 - Control de actuadores: luz (LED), ventilador, puerta (servo).  
-- Lectura de sensores en tiempo real (topic `smarteco/sensores`).
 """)
 
-st.info("Antes de usar: configura el broker MQTT en las páginas o en la barra lateral de Control/Voz/Imagen.")
 
-st.write("Si necesitas, mira el README para instrucciones de ejecución.")
